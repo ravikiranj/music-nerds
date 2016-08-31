cf32b1ce68f3f08b90da61814e493f50c72f179a
Ravikiran Janardhana <rjanardhana@tripadvisor.com>
2016-08-31 09:55:22 -0700

    Added directory creation and linkified images

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 # Fail on any error
 set -e
 
+# Create directories
+mkdir -p data/{filtered,models,raw,spotify_albums,spotify_tracks,youtube_videos}
+
 cd src/
 ./dump_room_history.py
 ./filter_music_messages.py
